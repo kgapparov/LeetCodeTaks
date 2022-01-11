fa46ad5123bf950936a962627b9d8a2ec4380089
khassangali <hasangaly@gmail.com>
2022-01-11 07:10:31 -0600

    Add FirstDuplicateValue task. Non Modified approach.

---
+++ src/leetcode/arrays/easy/medium/firstd_duplicate_value_LC287/FirstDuplicateValue.java
@@ -0,0 +1,51 @@
+package leetcode.arrays.easy.medium.firstd_duplicate_value_LC287;
+
+/**
+ * @author Khassangali Gapparov
+ * @Date  01/08/2022
+ * @version  0.0.1
+ * @target  F(M)ANG & Tesla!
+ */
+
+public class FirstDuplicateValue {
+    /*
+            we can approach in two ways:
+            a) first if in constraints says that we are not allowed modify array
+            and O(1) space, then we can go with turtle and hire method, slow and fast index traversing. Array values not
+            more than indexes, thats why we can traverse from value as index of next value.
+
+            b) if we can modify then we can jast modify visited elements with negotive value, if we face with negative
+            value while traversing then we found answer.
+         */
+
+    /**
+     * First without modifying input array.
+     * @param array input array
+     * @return return repeated value;
+     */
+    public static int firstDuplicateValue(int[] array) {
+        //fast and slow as hire and turtle approach.
+        int slow = array[0];
+        int fast = array[0];
+
+        slow = array[slow];
+        fast = array[array[fast]];
+
+        // traverse until slow and fast equals.
+        while (slow != fast) {
+            slow = array[slow];
+            fast = array[array[fast]];
+        }
+        // reset slow one, and where they meet again that value will be cyclic value. this time same speed.
+        slow = array[0];
+        while (slow != fast) {
+            slow = array[slow];
+            fast = array[fast];
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstDuplicateValue(new int[] {1,2,3,4,4,5,6}));
+    }
+}
